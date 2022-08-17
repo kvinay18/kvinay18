aa6af868f48ffe4e833f6a2efa2456582155c27f
kvinay18 <105418879+kvinay18@users.noreply.github.com>
2022-08-17 13:47:40 +0530

    GlobalSetting

---
+++ GlobalSettingController.cs
@@ -0,0 +1,34 @@
+﻿using AenonSrp.Application.Interfaces.Services.GlobalSetting;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AenonSrp.Server.Controllers.GlobaleSettings
+{
+
+    [Authorize]
+    [Route("api/GlobaleSettings/globalSetting")]
+    [ApiController]
+    public class GlobalSettingController : ControllerBase
+    {
+        private readonly IGlobalSettingService _globalSetting;
+        public GlobalSettingController(IGlobalSettingService globalSetting)
+        {
+            _globalSetting = globalSetting;
+        }
+
+        [AllowAnonymous]
+        [HttpGet("getGlobalSettingValueByName/{globalSettingName}")]
+        public async Task<ActionResult> GetGlobalSettingValueByName(string globalSettingName)
+        {
+            var globalSettingResponse = _globalSetting.GetGlobalSettingValueByName(globalSettingName);
+            return Ok(globalSettingResponse);
+        }
+
+        [AllowAnonymous]
+        [HttpPut("updateIactivityTime")]
+        public async Task<IActionResult> UpdateIactivityTime(int inactivityTime)
+        {
+            return Ok(_globalSetting.UpdateIactivityTime(inactivityTime));
+        }
+    }
+}
